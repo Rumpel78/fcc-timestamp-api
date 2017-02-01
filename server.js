800ae2f78b0a04b4f3a5eaec11ba188bc7b46212
Thomas Rzipa <th.rzipa@paprikon.at>
2017-02-01 11:58:05 +0000

    implemented solution to freecodecamp project

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express')
+var dateFormat = require('dateformat-light');
+
 var app = express()
 var port = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+function getDates(unknownDateType){ 
+  var date = new Date(+unknownDateType*1000);
+  if(isNaN(date.getTime())) //Date is not a unix timestamp
+  {
+      var date  = new Date(unknownDateType);
+  }
+  if(isNaN(date.getTime())) //Date is invalid
+  {
+      return { unix: null, natural: null }
+  }
+  var naturalDate = dateFormat(date, "mmmm dd, yyyy");
+  return { unix: date.getTime()/1000, natural: naturalDate }
+}
+
+app.get('/:time', function (req, res) {
+    var result = getDates(req.params.time)
+    res.send(JSON.stringify(result));
 })
 
 app.listen(port, function () {
