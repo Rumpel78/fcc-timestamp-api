553d8a4357e16c4db500e226abe8bf65775a0c4b
Thomas Rzipa <t.rzipa@adrom.at>
2017-10-31 14:44:25 +0100

    Move to pug

--- src/server.js
+++ src/server.js
@@ -2,25 +2,33 @@ var express = require('express')
 var dateFormat = require('dateformat-light');
 
 var app = express()
-var port = process.env.PORT || 8080;
+var port = process.env.VIRTUAL_PORT || 8080;
 
-function getDates(unknownDateType){ 
-  var date = new Date(+unknownDateType*1000);
-  if(isNaN(date.getTime())) //Date is not a unix timestamp
-  {
-      var date  = new Date(unknownDateType);
-  }
-  if(isNaN(date.getTime())) //Date is invalid
-  {
-      return { unix: null, natural: null }
-  }
-  var naturalDate = dateFormat(date, "mmmm dd, yyyy");
-  return { unix: date.getTime()/1000, natural: naturalDate }
+function getDates(unknownDateType) {
+    var date = new Date(+unknownDateType * 1000);
+    if (isNaN(date.getTime())) //Date is not a unix timestamp
+    {
+        var date = new Date(unknownDateType);
+    }
+    if (isNaN(date.getTime())) //Date is invalid
+    {
+        return { unix: null, natural: null }
+    }
+    var naturalDate = dateFormat(date, "mmmm dd, yyyy");
+    return { unix: date.getTime() / 1000, natural: naturalDate }
 }
 
 
+app.set('view engine', 'pug');
 
-app.use(express.static('public'));
+app.get('/', function (req, res) {
+    var baseUrl = req.protocol + ':' + port + '//' + req.host;
+
+    var mainDomain = req.host.substr(req.host.indexOf('.') + 1);
+    var baseHome = req.protocol + '://' + mainDomain + ':' + port
+
+    res.render('index', { host: baseUrl, home: baseHome });
+});
 
 app.get('/:time', function (req, res) {
     var result = getDates(req.params.time)
@@ -28,5 +36,5 @@ app.get('/:time', function (req, res) {
 })
 
 app.listen(port, function () {
-  console.log('Example app listening on port '+port)
+    console.log('Example app listening on port ' + port)
 })
